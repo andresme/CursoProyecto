7033e3f88aeeb4c8dadd686fff8a3ea619f7e790
andresme <andres.me@gmail.com>
2013-03-29 12:05:39 -0600

    added zoom better pan

--- TEClinica/src/com/itcr/clinica/activities/MapActivity.java
+++ TEClinica/src/com/itcr/clinica/activities/MapActivity.java
@@ -2,21 +2,16 @@ package com.itcr.clinica.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.GestureDetector.OnGestureListener;
 import android.view.Menu;
-import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.itcr.clinica.R;
 import com.itcr.custom.helpers.MapView;
-import com.itcr.custom.helpers.ThreadMapLoader;
 
-public class MapActivity extends Activity implements OnGestureListener{
+public class MapActivity extends Activity {
 
-	private GestureDetector gestureScanner;
-	private MapView mapView;
+	private MapView mMapView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +21,8 @@ public class MapActivity extends Activity implements OnGestureListener{
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-		mapView = new MapView(this);
-		setContentView(mapView);
-		gestureScanner = new GestureDetector(this, this);
+		mMapView = new MapView(this);
+		setContentView(mMapView);
 	}
 
 	@Override
@@ -38,69 +32,4 @@ public class MapActivity extends Activity implements OnGestureListener{
 		return true;
 	}
 
-	@Override
-	protected void onPause(){
-		super.onPause();
-		ThreadMapLoader.running = false;
-	}
-
-	@Override 
-	protected void onResume(){
-		super.onResume();
-		MapView.loader = new ThreadMapLoader();
-		MapView.loader.execute();
-	}
-
-	@Override
-	protected void onDestroy(){
-		super.onDestroy();
-		ThreadMapLoader.running = false;
-	}
-
-	@Override
-	public boolean onTouchEvent(MotionEvent me){
-		return gestureScanner.onTouchEvent(me);
-	}
-
-	@Override
-	public boolean onDown(MotionEvent arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean onFling(MotionEvent e1, MotionEvent e2, float x1,
-			float y1) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void onLongPress(MotionEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float x1,
-			float y1) {
-
-		MapView.xPosition += (int)(x1);
-		MapView.yPosition += (int)(y1);
-		mapView.invalidate();
-
-		return true;
-	}
-
-	@Override
-	public void onShowPress(MotionEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean onSingleTapUp(MotionEvent arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 }
--- TEClinica/src/com/itcr/custom/helpers/MapView.java
+++ TEClinica/src/com/itcr/custom/helpers/MapView.java
@@ -1,49 +1,160 @@
 package com.itcr.custom.helpers;
 
-
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
-import android.widget.ImageView;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
 
-public class MapView extends ImageView {
+import com.itcr.custom.utils.Utils;
 
-	public static BitmapDrawable map;
-	public static ThreadMapLoader loader;
-	public static Context ct;
-	public static int xPosition;
-	public static int yPosition;
+public class MapView extends View {
+	private static final int INVALID_POINTER_ID = -1;
+
+	private BitmapDrawable mMap;
+	private float mPosX;
+	private float mPosY;
+	private float mMin;
+	private DisplayMetrics mMetrics;
+	
+	private int mSizeX;
+	private int mSizeY;
+
+	private int mActivePointerId = INVALID_POINTER_ID;
+	private float mLastTouchX;
+	private float mLastTouchY;
+
+	private ScaleGestureDetector mScaleDetector;
+	private float mScaleFactor = 1.f;
 
 
 
-	public MapView(Context context, AttributeSet attrs) {
-		super(context, attrs);
+	public MapView(Context context) {
+		this(context, null, 0);
 		sharedConstructor(context);
 	}
 
-	public MapView(Context context){
-		super(context);
+	public MapView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+		sharedConstructor(context);
+	}
+
+	public MapView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
 		sharedConstructor(context);
 	}
 
 	public void sharedConstructor(Context context){
-		ct = context;
+		mSizeX = Utils.getWidth(this.getContext(), "mapa.png");
+		mSizeY = Utils.getHeight(this.getContext(), "mapa.png");
+		mMap = Utils.getBitmapFromAsset(this.getContext(), "mapa.png", 0, 0, mSizeX, mSizeY);
+		mMetrics = new DisplayMetrics();
+		((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(mMetrics);
+		float Xpx = mMetrics.widthPixels;
+		float Ypy = mMetrics.heightPixels;
+		mMin = Math.max((Xpx/mSizeX), (Ypy/mSizeY));
 		
-		xPosition = 0;
-		yPosition = 0;
+	}
 
-		loader = new ThreadMapLoader();
-		loader.execute();
+	@Override
+	public boolean onTouchEvent(MotionEvent ev){
+		// Let the ScaleGestureDetector inspect all events.
+		mScaleDetector.onTouchEvent(ev);
+
+		final int action = ev.getAction();
+		switch (action & MotionEvent.ACTION_MASK) {
+		case MotionEvent.ACTION_DOWN: {
+			final float x = ev.getX();
+			final float y = ev.getY();
+
+			mLastTouchX = x;
+			mLastTouchY = y;
+			mActivePointerId = ev.getPointerId(0);
+			break;
+		}
+
+		case MotionEvent.ACTION_MOVE: {
+			final int pointerIndex = ev.findPointerIndex(mActivePointerId);
+			final float x = ev.getX(pointerIndex);
+			final float y = ev.getY(pointerIndex);
+
+			// Only move if the ScaleGestureDetector isn't processing a gesture.
+			if (!mScaleDetector.isInProgress()) {
+				final float dx = x - mLastTouchX;
+				final float dy = y - mLastTouchY;
+
+				mPosX += dx;
+				mPosY += dy;
+				if(mPosX > 0) mPosX = 0;
+				if(mPosY > 0) mPosY = 0;
+				if(mPosX < (-1*mSizeX*mScaleFactor)+mMetrics.widthPixels) mPosX = (-1*mSizeX*mScaleFactor)+mMetrics.widthPixels;
+				if(mPosY < (-1*mSizeY*mScaleFactor)+mMetrics.heightPixels) mPosY = (-1*mSizeY*mScaleFactor)+mMetrics.heightPixels;
+
+				invalidate();
+			}
+
+			mLastTouchX = x;
+			mLastTouchY = y;
+
+			break;
+		}
+
+		case MotionEvent.ACTION_UP: {
+			mActivePointerId = INVALID_POINTER_ID;
+			break;
+		}
+
+		case MotionEvent.ACTION_CANCEL: {
+			mActivePointerId = INVALID_POINTER_ID;
+			break;
+		}
+
+		case MotionEvent.ACTION_POINTER_UP: {
+			final int pointerIndex = (ev.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK) 
+					>> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+					final int pointerId = ev.getPointerId(pointerIndex);
+					if (pointerId == mActivePointerId) {
+						// This was our active pointer going up. Choose a new
+						// active pointer and adjust accordingly.
+						final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+						mLastTouchX = ev.getX(newPointerIndex);
+						mLastTouchY = ev.getY(newPointerIndex);
+						mActivePointerId = ev.getPointerId(newPointerIndex);
+					}
+					break;
+		}
+		}
 
 
+		return true;
 	}
 
 	@Override
 	public void onDraw(Canvas g){
 		super.onDraw(g);
-		g.translate(-xPosition, -yPosition);
-		map.draw(g);
+		g.translate(mPosX, mPosY);
+		g.scale(mScaleFactor, mScaleFactor);
+
+		mMap.draw(g);
+		g.restore();
+	}
+
+	private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+		@Override
+		public boolean onScale(ScaleGestureDetector detector) {
+			mScaleFactor *= detector.getScaleFactor();
+
+			// Don't let the object get too small or too large.
+			mScaleFactor = Math.max(mMin, Math.min(mScaleFactor, 1.5f));
+
+			invalidate();
+			return true;
+		}
 	}
 
 }
