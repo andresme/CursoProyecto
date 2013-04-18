982bb8151e613f4db1701e8b155a5f9de8926a6d
Irene <irene@irene-laptop.(none)>
2013-04-17 18:02:58 -0600

    DataStructure Contact

---
+++ TEClinica/src/com/itcr/datastructures/Contact.java
@@ -0,0 +1,57 @@
+package com.itcr.datastructures;
+
+public class Contact {
+	private long id;
+	private String nameContact;
+	private String phoneOffice;
+	private String cell;
+	private String mail;
+	//private String service; ---> poner un fk en la tabla con el id del servicio
+	
+	public long getID(){
+		return id;		
+	}
+	
+	public void setID(long id){
+		this.id = id;
+	}
+	
+	public String getNameContact(){
+		return nameContact;
+	}
+	
+	public void setNameContact(String name){
+		this.nameContact = name;
+	}
+	
+	public String getPhoneOffice(){
+		return phoneOffice;
+	}
+	
+	public void setPhoneOffice(String phone){
+		this.phoneOffice = phone;
+	}
+	
+	public String getCell(){
+		return cell;
+	}
+	
+	public void setCell(String cell){
+		this.cell = cell;
+	}
+	
+	public String getMail(){
+		return mail;
+		
+	}
+	
+	public void setMail(String mail){
+		this.mail = mail;
+	}
+
+	// Will be used by the ArrayAdapter in the ListView
+	@Override
+	public String toString(){
+		return nameContact + phoneOffice + cell + mail;
+	}
+}
