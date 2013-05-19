d72c3598bd457704b719bb712e4683c1d4ded676
andresme <andres.me@gmail.com>
2013-05-18 21:48:28 -0600

    notifications and design improvement

--- TEClinica/src/com/itcr/custom/helpers/AddAppointmentDialog.java
+++ TEClinica/src/com/itcr/custom/helpers/AddAppointmentDialog.java
@@ -1,14 +1,24 @@
 package com.itcr.custom.helpers;
 
+import android.app.AlarmManager;
 import android.app.Dialog;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.View;
 import android.widget.*;
 import com.itcr.clinica.R;
 import com.itcr.clinica.activities.ScheduleListActivity;
 import com.itcr.custom.sqlite.DataSourceService;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class AddAppointmentDialog extends DialogFragment {
@@ -53,7 +63,7 @@ public class AddAppointmentDialog extends DialogFragment {
 
                 String name = nameET.getText().toString();
                 String description = descriptionET.getText().toString();
-                String date = dateP.getDayOfMonth() + "/" + dateP.getMonth() + "/" + dateP.getYear();
+                String date = dateP.getDayOfMonth() + "/" + (dateP.getMonth()+1) + "/" + dateP.getYear();
                 String time = timeP.getCurrentHour() + ":" + timeP.getCurrentMinute();
 
                 if (name.equals("") || description.equals("") || date.equals("") || time.equals("")) {
@@ -61,10 +71,11 @@ public class AddAppointmentDialog extends DialogFragment {
                     Toast addToast = Toast.makeText(mContext, message, Toast.LENGTH_SHORT);
                     addToast.show();
                 } else {
-                    mDatasource.createAppointment(name, description, date + " - " + time);
+                    int id = (int) mDatasource.createAppointment(name, description, date + " - " + time).getId();
                     String message = getResources().getString(R.string.appointment_toast_form_accept);
                     Toast addToast = Toast.makeText(mContext, message, Toast.LENGTH_SHORT);
                     addToast.show();
+                    addNotification(date + " - " + time, id);
                     if(mActivity != null)
                         mActivity.loadAppointments();
                 }
@@ -72,6 +83,26 @@ public class AddAppointmentDialog extends DialogFragment {
             }
         });
         return addAppointment;
+
+    }
+
+    public void addNotification(String date, int id){
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy - HH:mm");
+        Date date_;
+        try {
+            date_ = dateFormat.parse(date);
+            calendar.setTimeInMillis(date_.getTime());
+            AlarmManager alarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+            Intent intent = new Intent(mContext, NotificationReceiver.class);
+            intent.setData(Uri.parse("timer:" + id));
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(mContext, id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        } catch (ParseException e) {
+            Log.e("ParseException","could not parse"+date);
+        }
+
+
 
     }
 }
